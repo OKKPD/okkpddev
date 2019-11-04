630012bed76e813cd64f3cc1b65381e17da39695
adi.zulfikar <adi.zulfikar@mavila.co.id>
2019-11-04 23:01:11 +0700

    Layanan PSAT

--- okkpd_mobile/lib/model/repository/produkRepo.dart
+++ okkpd_mobile/lib/model/repository/produkRepo.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-
 import 'package:okkpd_mobile/constants/key.dart';
 import 'package:http/http.dart' as http;
+import 'package:okkpd_mobile/model/produkModel.dart';
 import 'package:okkpd_mobile/model/responseModel.dart';
 import 'package:okkpd_mobile/tools/GlobalFunction.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -19,6 +19,24 @@ class ProdukRepo {
         headers: {"Content-Type": "application/json"},
         body: json.encode(produks));
 
+    var resp = ResponseModel.fromJson(json.decode(response.body));
+    FunctionDart().setToast(resp.message);
+    if (response.statusCode != 200) {
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
+  Future<bool> postProduk(List<ProdukModel> export, String jenis) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var idUsaha = prefs.getString('loginidUsaha');
+
+    print(json.encode(export));
+    var url = '${Keys.APIURL}layanan/$idUsaha/daftar/$jenis';
+    var response = await http.post(url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(export));
 
     var resp = ResponseModel.fromJson(json.decode(response.body));
     FunctionDart().setToast(resp.message);
